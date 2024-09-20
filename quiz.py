67c3d2581dcce54448571185356c48aa174813fd
Ahum Maitra <130306342+AhumMaitra@users.noreply.github.com>
2024-09-20 22:48:59 +0530

    Create quiz.py

---
+++ quiz.py
@@ -0,0 +1,14 @@
+print("Welcome to this basic Quiz Game, developed by Ahum Maitra")
+let's_start = input("Do you want to play this game? ")
+
+if let's_start == "Yes":
+    a = input("Who developed Python? ")
+    if a == "Guido van Rossum":
+        print("Right answer!")
+    else:
+        print("Wrong answer")
+    b= input("When was the first version of Python?)
+         if b== "February 20, 1991":
+             print (Right answer")
+         else:
+             print ("Wrong Answer")
